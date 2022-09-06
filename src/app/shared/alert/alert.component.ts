60c3bf5523ff9603130f394b1a85004206eabfa2
Serhii Korniichuk <skorniychyk@gmail.com>
2022-09-06 18:07:01 +0300

    ADD: creating new user with Firebase

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,14 +1,24 @@
 import { Component, Input } from '@angular/core'
 
+export type IAlertTypes = 'info' | 'success' | 'error'
+
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss']
 })
 export class AlertComponent {
-  @Input() color = 'sky'
+  @Input() alertType: IAlertTypes = 'info'
 
   get bgColor() {
-    return `bg-${this.color}-500`
+    switch (this.alertType) {
+      case 'success':
+        return 'bg-green-500'
+      case 'error':
+        return 'bg-red-500'
+      case 'info':
+      default:
+        return 'bg-sky-500'
+    }
   }
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { IAlertTypes } from 'src/app/shared/alert/alert.component'
 
 @Component({
   selector: 'app-register',
@@ -7,6 +9,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  constructor(private authService: AngularFireAuth) {}
+
+  isSubmission = false
+
   name = new FormControl('', [
     Validators.required,
     Validators.minLength(2),
@@ -36,12 +42,31 @@ export class RegisterComponent {
 
   showAlert = false
   alertMessage = ''
-  alertColor = 'sky'
+  alertType: IAlertTypes = 'info'
 
-  register() {
+  async register() {
+    this.isSubmission = true
     this.showAlert = true
     this.alertMessage = 'Please wait! Your account is being created.'
-    console.log(this.registerForm.value)
+
+    const { email, password } = this.registerForm.value
+
+    try {
+      const userCredentials =
+        await this.authService.createUserWithEmailAndPassword(email!, password!)
+      console.log('userCredentials', userCredentials)
+      this.isSubmission = false
+    } catch (e) {
+      console.error(e)
+      this.isSubmission = false
+      this.alertType = 'error'
+      this.alertMessage = 'An unexpected error occured. Please try again later.'
+      return
+    }
+
+    this.alertType = 'success'
+    this.alertMessage = 'Success! Your account has been created.'
+
     this.registerForm.reset()
   }
 }
