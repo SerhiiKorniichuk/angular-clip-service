e2f95e13d9954925d01c420ae639397b56ed882f
Serhii Korniichuk <skorniychyk@gmail.com>
2022-09-13 18:13:23 +0300

    ADD: AuthService in register method

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
-import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { AuthService } from 'src/app/services/auth/auth.service'
 import { IAlertTypes } from 'src/app/shared/alert/alert.component'
 
 @Component({
@@ -9,7 +9,7 @@ import { IAlertTypes } from 'src/app/shared/alert/alert.component'
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  constructor(private authService: AngularFireAuth) {}
+  constructor(private authService: AuthService) {}
 
   isSubmission = false
 
@@ -45,28 +45,39 @@ export class RegisterComponent {
   alertType: IAlertTypes = 'info'
 
   async register() {
+    // Add lock for Submit button
     this.isSubmission = true
+    // Show alert with info message
     this.showAlert = true
+    this.alertType = 'info'
     this.alertMessage = 'Please wait! Your account is being created.'
 
-    const { email, password } = this.registerForm.value
-
     try {
-      const userCredentials =
-        await this.authService.createUserWithEmailAndPassword(email!, password!)
-      console.log('userCredentials', userCredentials)
+      // Creating a new user
+      await this.authService.createUser({
+        name: this.name.value!,
+        email: this.email.value!,
+        phoneNumber: this.phoneNumber.value!,
+        age: this.age.value!,
+        password: this.password.value!
+      })
+      // Remove lock for Submit button
       this.isSubmission = false
     } catch (e) {
       console.error(e)
+      // Remove lock for Submit button
       this.isSubmission = false
+      // Show alert with error message
       this.alertType = 'error'
       this.alertMessage = 'An unexpected error occured. Please try again later.'
       return
     }
 
+    // Show alert with success message
     this.alertType = 'success'
     this.alertMessage = 'Success! Your account has been created.'
 
+    // Reset form
     this.registerForm.reset()
   }
 }
